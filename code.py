28cb267dcf584e81aeda96705fb9def73976a5f3
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-05-23 23:44:55 +0530

    even no

--- code.py
+++ code.py
@@ -6,10 +6,20 @@ greet()
 def square(x):
     return x*x
 x= square(17)
-print(x)'''
+print(x)
 
 def greet(name):
     print("Hello: ", name)
 greet("Debasrita")
 greet("Jit")
-greet("Tarunima")
+greet("Tarunima")'''
+
+def count_even(n):
+    count=0
+    for i in range(1,n+1):
+        if i%2==0:
+            print(i)
+            count+=1
+    return count
+result= count_even(10)
+print(result)
