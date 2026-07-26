3fcc2168ac7d713a172b54e2578d4b4120f6c977
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-07-26 22:52:25 +0530

    1..

---
+++ Abstract.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+# Abstract Class
+class Animal(ABC):
+
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+# Child Class
+class Dog(Animal):
+
+    def sound(self):
+        print("Dog says Bow Bow")
+
+
+# Creating object
+d = Dog()
+
+d.sound()
