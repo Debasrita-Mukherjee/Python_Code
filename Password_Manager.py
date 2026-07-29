5d0ce48184e6dd0860768a439040516f66727d5e
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-07-29 12:23:43 +0530

    9.py

--- Password_Manager.py
+++ Password_Manager.py
@@ -6,7 +6,7 @@ passwords= {}
 
 #Load existing password file
 try:
-    with open("Password.txt","r") as file:
+    with open("passwords.txt","r") as file:
         for line in file:
             website, pwd = line.strip().split(":")
             passwords[website]=pwd
