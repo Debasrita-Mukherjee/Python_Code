0c3b71e7a7dae31e7d3211dcc5904f485dc06c91
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-07-30 23:11:22 +0530

    numpy4

--- zeroesNUM.py
+++ zeroesNUM.py
@@ -1,4 +1,10 @@
 import numpy as np
 
-zeros_array = np.zeros(3)
-print(zeros_array)
+# zeros_array = np.zeros(3)
+# print(zeros_array)
+
+# ones_array = np.ones((2,3))
+# print(ones_array)
+
+filles_array = np.full((2,2),7)
+print(filles_array)
