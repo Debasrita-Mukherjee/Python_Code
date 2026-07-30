2f4d6188407d5e7c63848530da6602048e58647f
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-07-30 23:14:35 +0530

    numpy5

--- zeroesNUM.py
+++ zeroesNUM.py
@@ -3,8 +3,11 @@ import numpy as np
 # zeros_array = np.zeros(3)
 # print(zeros_array)
 
-# ones_array = np.ones((2,3))
-# print(ones_array)
+ones_array = np.ones((2,3))
+print(ones_array)
 
-filles_array = np.full((2,2),7)
-print(filles_array)
+# filles_array = np.full((2,2),7)
+# print(filles_array)
+
+# arr = np.arange(1,10,2)
+# print(arr)
