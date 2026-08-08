bfb1614d91228071de3b835aff70c0219ad191c1
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-08-08 22:14:33 +0530

    pandas5

--- pandas/csvjson.py
+++ pandas/csvjson.py
@@ -1,5 +1,5 @@
 import pandas as pandas
 
-df = pd.read_csv("C:\Users\Debasrita Mukherjee\OneDrive\Desktop\Python_code\pandas\csvjson.py", encoding = "latin1")
+df = pd.read_csv("C:\Users\Debasrita Mukherjee\OneDrive\Desktop\Python_code\pandas\csvjson.py", encoding = "utf-8")
 
 print(df)
