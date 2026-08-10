fbd62583cef6f3b1525300d8c98917eeb4194373
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-08-10 19:31:10 +0530

    11

--- pandas/save.py
+++ pandas/save.py
@@ -8,6 +8,10 @@ data = {
 
 df = pd.DataFrame(data)
 
-#print(df)
+print(df)
 
-df.to_csv("data.csv", index = False)
+df.to_csv("output.csv", index = False)
+
+df.to_excel("output.xlsx", index = False)
+
+df.to_json("output.json", index = False)
