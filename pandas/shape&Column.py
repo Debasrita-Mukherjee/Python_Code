a025ed56b5cf5f7b741d2cc501242d1b38e82242
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-08-11 23:53:28 +0530

    pandas7

---
+++ pandas/shape&Column.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = {
+    "Name" : ['Ram', 'Shyam', 'Ghanashyam', 'Radha', 'Rani', 'Bani', 'Chiru', 'Ravi', 'Soma'],
+    "Age" : [10,20,30,40, 50, 60, 70, 80, 90],
+    "City" : ['Nagpur', 'Kolkata', 'Delhi', 'Mumbai', 'Bengalore', 'Pune', 'Jaipur', 'Kota', 'Jalpaiguri'],
+    "Salary": [50000, 85000, 95000, 68000, 70000, 90000, 100000, 150000, 60000]
+}
+
+df = pd.DataFrame(data)
+print(df)
+print("f'shape: {df.shape}")
+print("f'Column_Names: {df.columns}")
