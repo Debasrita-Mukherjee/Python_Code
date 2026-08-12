e038e59e2598b378cf95fd36217533afd3696f9c
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-08-12 22:16:59 +0530

    pandas12

--- pandas/multiple_columns.py
+++ pandas/multiple_columns.py
@@ -6,3 +6,7 @@ data = {
     "City" : ['Nagpur', 'Kolkata', 'Delhi', 'Mumbai', 'Bengalore', 'Pune', 'Jaipur', 'Kota', 'Jalpaiguri'],
     "Salary": [50000, 85000, 95000, 68000, 70000, 90000, 100000, 150000, 60000]
 }
+
+subset = df[["Name"], ["Salary"]]
+print("\n Subset with Name and Salary: ")
+Print(subset)
