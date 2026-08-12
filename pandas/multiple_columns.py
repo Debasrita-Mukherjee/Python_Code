b45d2a5ba1d6e958c54adcf0de538f51ee4fabb4
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-08-12 22:12:50 +0530

    pandas11

---
+++ pandas/multiple_columns.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+data = {
+    "Name" : ['Ram', 'Shyam', 'Ghanashyam', 'Radha', 'Rani', 'Bani', 'Chiru', 'Ravi', 'Soma'],
+    "Age" : [10,20,30,40, 50, 60, 70, 80, 90],
+    "City" : ['Nagpur', 'Kolkata', 'Delhi', 'Mumbai', 'Bengalore', 'Pune', 'Jaipur', 'Kota', 'Jalpaiguri'],
+    "Salary": [50000, 85000, 95000, 68000, 70000, 90000, 100000, 150000, 60000]
+}
