0785d58e3de9a12c555182a44894f6153b8414ad
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-08-12 22:20:51 +0530

    pandas13

--- pandas/multiple_columns.py
+++ pandas/multiple_columns.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
 data = {
-    "Name" : ['Ram', 'Shyam', 'Ghanashyam', 'Radha', 'Rani', 'Bani', 'Chiru', 'Ravi', 'Soma'],
-    "Age" : [10,20,30,40, 50, 60, 70, 80, 90],
-    "City" : ['Nagpur', 'Kolkata', 'Delhi', 'Mumbai', 'Bengalore', 'Pune', 'Jaipur', 'Kota', 'Jalpaiguri'],
+    "Name": ['Ram', 'Shyam', 'Ghanashyam', 'Radha', 'Rani', 'Bani', 'Chiru', 'Ravi', 'Soma'],
+    "Age": [10, 20, 30, 40, 50, 60, 70, 80, 90],
+    "City": ['Nagpur', 'Kolkata', 'Delhi', 'Mumbai', 'Bengalore', 'Pune', 'Jaipur', 'Kota', 'Jalpaiguri'],
     "Salary": [50000, 85000, 95000, 68000, 70000, 90000, 100000, 150000, 60000]
 }
 
-subset = df[["Name"], ["Salary"]]
-print("\n Subset with Name and Salary: ")
-Print(subset)
+df = pd.DataFrame(data)
+
+subset = df[["Name", "Salary"]]
+
+print("\nSubset with Name and Salary:")
+print(subset)
