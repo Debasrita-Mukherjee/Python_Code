1933bc30b029c7845e1c0c1b5f9858d2357201c4
Debasrita-Mukherjee <debasritamukherjee785@gmail.com>
2026-08-12 23:26:09 +0530

    pandas16

---
+++ pandas/tempCodeRunnerFile.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+data = {
+    "Name": ['Ram', 'Shyam', 'Ghanashyam', 'Radha', 'Rani', 'Bani', 'Chiru', 'Ravi', 'Soma'],
+    "Age": [10, 20, 30, 40, 50, 60, 70, 80, 90],
+    "City": ['Nagpur', 'Kolkata', 'Delhi', 'Mumbai', 'Bengalore', 'Pune', 'Jaipur', 'Kota', 'Jalpaiguri'],
+    "Salary": [50000, 85000, 95000, 68000, 70000, 90000, 100000, 150000, 60000]
+}
+
+df = pd.DataFrame(data)
+
+high_salary = df[df['Salary']>50000]
+
+print("Employees with Salary > 50000: ")
+print(high_salary)
+
+filtered = df[(df[Age] > 30) & (df['Salary'] > 50000)]
+print(f'Employee list Age > 30 + Salary >50000')
+print(filtered)
